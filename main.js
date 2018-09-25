b24a1b2ca96a8f204ae0f8d15fba3840779847c9
fred <fredgrei@gmail.com>
2018-09-25 11:37:31 +0200

    hmm

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var click = 0;
 
+
 function normalClick(number){
 	click = click + number;
 	document.getElementById("click").innerHTML = click;
 };
+
+
+function upgradeFirst(){
+	click += 100;
+	console.log(kek);
+}
