d55f0bae130c37b74debf8a732e01441c56fe771
fred <fredgrei@gmail.com>
2018-10-01 19:28:42 +0200

    html til php

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
 
-
 <html lang="no">
 	<head>
-		<link rel="stylesheet" type="text/css" href="">
+		<title>KlappeSpill</title>
+		<link rel="stylesheet" type="text/css" href="interface.css" />
 		<meta charset="utf-8"/>
+
 	</head>
 	<body>
-		<h1> Herru you</h1>
-		<p style="
-    		width: 50%;
-    		background-color: #00BCD4;
-			"> #Antall klikk: <span style="color:blue;font-weight:bold" id="click"> </span> </p>
-			
-		<button type="click" onclick="normalClick(1)">Trykk her</button>
-		<script type="text/javascript" src="main.js"></script>
+		<h1> Herru you, klappe?!</h1>
+		<ul>
+			<li><a href="meny.html">Upgrades </a></li>
+		</ul>
+			<img src="https://loremflickr.com/320/240" alt="placeholder"/>
+			<p style="
+	    		width: 50%;
+	    		background-color: #00BCD4;
+				"> #Klapp: <span style="color:blue;font-weight:bold" id="click"> </span> </p>
+			<button type="click" onclick="normalClick(1)">Klapp pusen</button>
+			<button type="upgrade" onclick="upgradeFirst()" > 
+			Klappere: <span id="cursors">0</span> <br />
+			Pris: <span id="cursorsCost">10</span>
+			</button>
+		</br>
+			<script type="text/javascript" src="main.js"></script>
 	</body>
 	</html>
