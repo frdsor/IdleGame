779dd2d3f07e1e523f7d7a2b7ad5a098154631e9
fred <fredgrei@gmail.com>
2018-10-02 12:42:29 +0200

    bokser

--- index.php
+++ index.php
@@ -8,20 +8,26 @@
 	</head>
 	<body>
 		<h1> Herru you, klappe?!</h1>
+		<div class="min-boks">Klapp katten<br />
 		<ul>
 			<li><a href="meny.html">Upgrades </a></li>
+			<li><a href="#">Stats </a></li>
 		</ul>
+	</div>
+		<div class="boks2"> 
 			<img src="https://loremflickr.com/320/240" alt="placeholder"/>
 			<p style="
 	    		width: 25%;
 	    		background-color: powderblue;
 				"> #Klapp: <span style="color:blue;font-weight:bold" id="click"> </span> </p>
+
 			<button type="click" onclick="normalClick(1)">Klapp pusen</button>
 			<button type="upgrade" onclick="upgradeFirst()" > 
 			#Klappere: <span id="cursors">0</span> <br />
 			Pris: <span id="cursorsCost">10</span>
 			</button>
 		</br>
+		</div>
 			<script type="text/javascript" src="main.js"></script>
 	</body>
 	</html>
