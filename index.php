67430cf87b465f69f7447b02380a182d73f8f301
fred <fredgrei@gmail.com>
2018-10-06 21:04:57 +0200

    centerd

--- index.php
+++ index.php
@@ -8,29 +8,47 @@
 	</head>
 	<body>
 		<h1> Herru you, klappe?!</h1>
-		<div class="min-boks">Klapp katten<br />
+		<br />
+		<div class="min-boks"> 
 		<ul>
 			<li><a href="meny.html">Upgrades </a></li>
-			<li><a href="#">Stats </a></li>
+			<li><a href="stats.html">Stats </a></li>
+
 		</ul>
 	</div>
-		<div class="boks2"> 
+	<br />
+		<div class="boks2">
+		<br /> 
 			<img src="https://loremflickr.com/320/240" alt="placeholder"/>
-			<p style="width: 25%; background-color: powderblue;">
+			<p style="width: 25%; background-color: powderblue; text-align: center;">
 			#Klapp: <span style="color:blue;font-weight:bold" id="click"> </span> </p>
-
-			<button type="click" onclick="normalClick(1)">Klapp pusen</button>
-			<button type="upgrade" onclick="upgradeFirst()"> 
-			#Klappere: <span id="cursors">0</span> <br />
-			Pris: <span id="cursorsCost">10</span>
-			</button>
+			<div class ="klikkeButtons">
+				<button type="click" onclick="normalClick(1)">Klapp pusen </button>
+				<button type="upgrade" onclick="upgradeFirst()"> 
+				#Klappere: <span id="klappere">0</span> <br />
+				Pris: <span id="klappereCost">10</span>
+				</button>
+			</div>
 		</br>
 		</div>
+		<br />
 		<div class ="save">
 			<button class="saveLoad" onclick="save()">Save</button>
 			<button class="saveLoad" onclick="load()"> Load</button> 
-			<button class="saveLoad" onclick="reset()">Reset</button>
+			<button class="reset" onclick="reset()">Reset</button>
 		</div>
 			<script type="text/javascript" src="main.js"></script>
 	</body>
 	</html>
+
+
+	<!-- TODO: 
+		Upgrade:	Kattemat - Klikkeklapper blir bedre.
+					Katteleker Ende bedre klappere. 4 til normalClick elns
+					Kattehus - Flere katter (?)
+
+		Endre desimaler/tall
+		Bytte kattebilde ved X klapp
+		Achivmentzorz 
+	-->
+
--- main.js
+++ main.js
@@ -1,31 +1,42 @@
 var click = 0;
-var cursors = 0;
+var klappere = 0;
+var totalClick = 0;
 
 
 
 function normalClick(number){
 	click = click + number;
-	document.getElementById("click").innerHTML = click;
+	document.getElementById("click").innerHTML = prettify(click);
 };
+
+function getTotal(){
+	console.log("kek");
+	document.getElementById("totalClick").innerHTML = prettify(totalClick);
+}
 
 
 function upgradeFirst(){
-	var cursorsCost = Math.floor(10  * Math.pow(1.1,cursors));
-	if(click >= cursorsCost){
-		cursors = cursors + 1;
-		click = click - cursorsCost;
-		document.getElementById('cursors').innerHTML = cursors;
-		document.getElementById('click').innerHTML = click;
+	var klappereCost = Math.floor(10  * Math.pow(1.1,klappere));
+	if(click >= klappereCost){
+		klappere = klappere + 1;
+		click = click - klappereCost;
+		document.getElementById('klappere').innerHTML = prettify(klappere);
+		document.getElementById('click').innerHTML = prettify(click);
 	};
-	var nextCost = Math.floor(10* Math.pow(1.1,cursors));
-	document.getElementById("cursorsCost").innerHTML = nextCost;
+	var nextCost = Math.floor(10* Math.pow(1.1,klappere));
+	document.getElementById("klappereCost").innerHTML = prettify(nextCost);
 };
+
+function prettify(number){
+    var output = Math.round(number * 1000000)/1000000;
+	return output;
+}
 
 function save(){
 	var save = {
 		click: click,
-		cursors: cursors,
-		cursorsCost: cursorsCost
+		klappere: klappere,
+		klappereCost: klappereCost
 	}
 	localStorage.setItem("save",JSON.stringify(save));
 }
@@ -35,11 +46,11 @@ function load(){
 	if (typeof savegame.click !== "undefined"){
 	 click = savegame.click;
 	}
-	if (typeof savegame.cursors !== "undefined"){
-	 cursors = savegame.cursors;
+	if (typeof savegame.klappere !== "undefined"){
+	 klappere = savegame.klappere;
 	 }
-	 if(typeof savegame.cursorsCost !== "undefined"){
-	 	cursorsCost = savegame.cursorsCost;
+	 if(typeof savegame.klappereCost !== "undefined"){
+	 	klappereCost = savegame.klappereCost;
 	 }
 }
 
@@ -48,5 +59,5 @@ function reset(){
 }
 
 window.setInterval(function(){
-normalClick(cursors);
+normalClick(klappere);
 }, 1000);
